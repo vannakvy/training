8fbcd400b90c1ffb628c922868fc50e8ecc6c8b7
vannakvy <vannakvy2020@gmail.com>
2022-04-27 14:47:56 +0700

    this is the first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// Requiring modules
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+var fs = require('fs');
+const port = 8000;
+
+// Render index.ejs file
+app.get('/', function (req, res) {
+
+	// Render page using renderFile method
+	ejs.renderFile('index.ejs', {},
+		{}, function (err, template) {
+		if (err) {
+			throw err;
+		} else {
+			res.end(template);
+		}
+	});
+});
+
+// Render index.ejs file
+app.get('/about', function (req, res) {
+	// Render page using renderFile method
+	ejs.renderFile('about.ejs', {},
+		{}, function (err, template) {
+		if (err) {
+			throw err;
+		} else {
+			res.end(template);
+		}
+	});
+});
+
+// Server setup
+app.listen(port, function (error) {
+	if (error)
+		throw error;
+	else
+		console.log("Server is running");
+});
